65f9f31c55d88caef3ef0be056f62403de4ec422
Breno Delgado <brenodelgado.dev@gmail.com>
2022-01-10 10:17:10 -0300

    Project Created

---
+++ backend/src/main/java/com/brenodev/brenomovie/BrenomovieApplication.java
@@ -0,0 +1,13 @@
+package com.brenodev.brenomovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrenomovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrenomovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brenodev/brenomovie/BrenomovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenodev.brenomovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrenomovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
